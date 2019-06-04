977a1476f2b4d6fad8625ca7f2532cac1c9b4434
Luc Van Den Handel <lucvdh@icloud.com>
2019-06-04 10:34:59 +0200

    Initial Commit

---
+++ Coffee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee
+//
+//  Created by Luc Van Den Handel on 2019/06/04.
+//  Copyright © 2019 Luc Van Den Handel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
